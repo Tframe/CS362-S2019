c1ca4da839a80e8934470be8e8002d9ac773bde5
Trevor Frame <framet@flip3.engr.oregonstate.edu>
2019-05-19 22:02:01 -0700

    Adding random test files

--- projects/framet/dominion/randomtestadventurer.c
+++ projects/framet/dominion/randomtestadventurer.c
@@ -15,66 +15,106 @@
 #include <assert.h>
 #include "rngs.h"
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
+
 
 #define TESTCARD "adventurer"
 
 
-int checkAdventurer(int currentPlayer, struct gameState *testState){
-	int drawntreasure = 0;
-	int cardDrawn;
-	int temphand[MAX_HAND];
-	int z = 0;
+int checkAdventurer(int currentPlayer, struct gameState *testG){
 
-	int testing;
+	int handpos = 0;
+	int choice1 = 0;
+	int choice2 = 0;
+	int choice3 = 0;
+	int bonus = 0;
+	
 
-	testing = playAdventurer(currentPlayer, testState, temphand, drawntreasure, z);
-
+	cardEffect(adventurer, choice1, choice2, choice3, testG, handpos, &bonus);
+	return 0;	
 }
 
 int main(){
 
-	srand(time(0));
 	int deckCount;
 	int discardCount;
 	int handCount;
 	int player;
-
+	int numPlayers = 4;
 	struct gameState G;
+	struct gameState testG;
+	int extraTreasures;
+	int numTests = 2000;
+	int numPass = 0;
+	int numFail = 0;	
 
-	//int k[10] = {adventurer, smithy, embargo, tribute, steward, mine, cutpurse, council_room, village, sea_hag};
-	int k[16] = {estate, duchy, province, copper, silver, gold, adventurer, smithy, embargo, tribute, steward, mine, cutpurse, council_room, village, sea_hag};
-	
+	SelectStream(2);
+	PutSeed(3);
+
+	int k[10] = {adventurer, smithy, embargo, tribute, steward, mine, cutpurse, council_room, village, sea_hag};
+	int t[3] = {copper, silver, gold};
+
+	int expectedCount = 0;
+	int actualCount = 0;
 
 	printf("********** TESTING CARD: %s **********\n", TESTCARD);
 
-	printf("Random Teseting");
+	printf("Random Teseting\n");
 
-	for (int i = 0; i < 2000; i++){
+	for (int i = 0; i < numTests; i++){
+		expectedCount = 0;
+		actualCount = 0;
+		//int seed = floor(Random() * 1000);	
+		int seed = 1000;
+		initializeGame(numPlayers, k, seed, &G);
 	
-		for(int j = 0; j < sizeof(struct gameState); j++){
-			((char*)&G)[j] = rand() * 256;
+		extraTreasures = floor(Random() * 5);
+		
+		//add random number of treasure cards
+		for(int j = 0; j < extraTreasures; j++){
+			gainCard(t[(int)floor(Random() * 3)], &G, 1, player);
 		}
-	
-		player = rand() % 2;
-		G.deckCount[player] = rand() % MAX_DECK;
-		G.discardCount[player] = rand() % MAX_DECK;
-		G.handCount[player] = rand() % MAX_HAND + 1;
-	
-		//setup deck
-		for(int j = 0; j < G.deckCount[player]; j++){
-			G.deck[player][j] = k[rand() % 16];
+		player = floor(Random() * 4);
+
+		G.deckCount[player] = floor(Random() * MAX_DECK);
+		G.discardCount[player] = floor(Random() * MAX_DECK);
+		G.handCount[player] = floor(Random() * MAX_HAND);
+
+		for (int j = 0; j < G.handCount[player]; j++){
+			if(G.hand[player][j] == copper || G.hand[player][j] == silver || G.hand[player][j] == gold){
+				actualCount++;
+			}
 		}
-	
-		//setup hand
-		for(int j = 0; j < G.handCount[player]; j++){
-			G.hand[player][j] = k[rand() % 16];
+		expectedCount = actualCount + 2;
+
+		memcpy(&testG, &G, sizeof(struct gameState));
+
+		for (int m = 0; m < testG.handCount[player]; m++){
+			printf("%d, ", testG.hand[player][m]);
 		}
 
-		checkAdventurer(player, &G);
-	
+
+		checkAdventurer(player, &testG);
+		actualCount = 0;	
+		for (int j = 0; j < G.handCount[player]; j++){
+			if(testG.hand[player][j] == copper || testG.hand[player][j] == silver || testG.hand[player][j] == gold){
+				actualCount++;
+			}
+		}	
+		
+		if(expectedCount == actualCount || G.handCount[player] == testG.handCount[player] - 1){
+			numPass++;
+		}
+		else{
+			numFail++;
+		}
 	}
 	
+	printf("Test Complete!\n");
+	printf("Number of tests run: %d\n", numTests);
+	printf("Number of test pass: %d\n", numPass);
+	printf("Number of test failures: %d\n", numFail);
 
 	return 0;
 }
--- projects/framet/dominion/randomtestcard1.c
+++ projects/framet/dominion/randomtestcard1.c
@@ -15,13 +15,87 @@
 #include <assert.h>
 #include "rngs.h"
 #include <stdlib.h>
+#include <math.h>
 
-#definte TESTCARD "smithy"
+#define TESTCARD "smithy"
+
+
+int checkSmithy(int currentPlayer, struct gameState *testG){
+	
+	int handpos = 0;
+	int choice1 = 0;
+	int choice2 = 0;
+	int choice3 = 0;
+	int bonus = 0;
+
+	return cardEffect(smithy, choice1, choice2, choice3, testG, handpos, &bonus);
+
+}
 
 
 int main(){
 
+	int deckCount;
+	int discardCount;
+	int handCount;
+	int player;
+	int numPlayers = 4;
+	struct gameState G;
+	struct gameState testG;
+	int extraTreasures;
+	int numTests = 2000;
+	int numPass = 0;
+	int numFail = 0;	
 
+	SelectStream(2);
+	PutSeed(3);
 
+	int k[10] = {adventurer, smithy, embargo, tribute, steward, mine, cutpurse, council_room, village, sea_hag};
+	int t[3] = {copper, silver, gold};
 
+	int expectedCount = 0;
+	int actualCount = 0;
+
+	printf("********** TESTING CARD: %s **********\n", TESTCARD);
+
+	printf("Random Teseting\n");
+
+	for (int i = 0; i < numTests; i++){
+		expectedCount = 0;
+		actualCount = 0;
+		//int seed = floor(Random() * 1000);	
+		int seed = 1000;
+		initializeGame(numPlayers, k, seed, &G);
+	
+		extraTreasures = floor(Random() * 5);
+		
+		//add random number of treasure cards
+		for(int j = 0; j < extraTreasures; j++){
+			gainCard(t[(int)floor(Random() * 3)], &G, 1, player);
+		}
+		player = floor(Random() * 4);
+
+		G.deckCount[player] = floor(Random() * MAX_DECK);
+		G.discardCount[player] = floor(Random() * MAX_DECK);
+		G.handCount[player] = floor(Random() * MAX_HAND);
+
+		memcpy(&testG, &G, sizeof(struct gameState));
+
+		checkSmithy(player, &testG);
+		
+		if(G.handCount[player]  == testG.handCount[player] || G.deckCount[player]  == testG.deckCount[player] || G.discardCount[player] == testG.deckCount[player]){
+			numPass++;
+		}
+		else{
+			numFail++;
+		}
+
+	}
+	
+	printf("Test Complete!\n");
+	printf("Number of tests run: %d\n", numTests);
+	printf("Number of test pass: %d\n", numPass);
+	printf("Number of test failures: %d\n", numFail);
+
+	return 0;
 }
